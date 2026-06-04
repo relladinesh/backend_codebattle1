e87a9c0bb064e04373544890c1eab1d8e508c80f
relladinesh <150235051+relladinesh@users.noreply.github.com>
2026-06-04 13:50:47 +0530

    Refactor Redis configuration to use REDIS_URL

--- backend/services/gateway/src/config/redis.js
+++ backend/services/gateway/src/config/redis.js
@@ -3,15 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASSWORD,
-
-  tls: {
-    rejectUnauthorized: false,
-  },
-
+export const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
 
